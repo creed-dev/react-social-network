e4e25f2d7c06a761c68009af31fe1f79aac8a295
Creed <creed-dev@internet.ru>
2021-08-04 23:05:09 +0700

    install axios and add get-request

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,58 +1,23 @@
+import * as axios from 'axios';
+import userAvatar from '../../assets/img/user-logo.png';
+
 const Users = props => {
 	if (props.users.length === 0) {
-		props.setUsers([
-			{
-				id: 1,
-				avatar:
-					'https://ru-static.z-dn.net/files/d5d/f71519847c439d7a1f96f9f9908ccaab.jpg',
-				followed: true,
-				fullName: {
-					name: 'Ilya',
-					surname: 'Chupin',
-				},
-				status: 'Ola!',
-				location: {
-					country: 'Russia',
-					city: 'Moscow',
-				},
-			},
-			{
-				id: 2,
-				avatar:
-					'https://ulibky.ru/wp-content/uploads/2019/10/avatarki_dlya_vatsap_dlya_devushek_42_28061027.jpg',
-				followed: false,
-				fullName: {
-					name: 'Pavel',
-					surname: 'Shapovalov',
-				},
-				status: 'Hello!',
-				location: {
-					country: 'Russia',
-					city: 'Novosibirsk',
-				},
-			},
-			{
-				id: 3,
-				avatar:
-					'https://demotivation.ru/wp-content/uploads/2020/06/s1200-28-10-1024x1024.jpg',
-				followed: true,
-				fullName: {
-					name: 'Alex',
-					surname: 'Korobkin',
-				},
-				status: 'Privet!',
-				location: {
-					country: 'Russia',
-					city: 'Karasuk',
-				},
-			},
-		]);
+		axios
+			.get('https://social-network.samuraijs.com/api/1.0/users')
+			.then(response => {
+				props.setUsers(response.data.items);
+			});
 	}
 
 	const renderUserCards = props.users.map(user => (
 		<div className="user">
 			<div className="user__action">
-				<img src={user.avatar} alt="avatar" className="user__avatar" />
+				<img
+					src={user.photos.small != null ? user.photos.small : userAvatar}
+					alt="avatar"
+					className="user__avatar"
+				/>
 				{user.followed ? (
 					<button
 						onClick={() => {
@@ -76,15 +41,17 @@ const Users = props => {
 			<div className="user__info">
 				<div className="user__descr">
 					<div className="user__descr-name">
-						{user.fullName.name}
-						&nbsp;
-						{user.fullName.surname}
+						{user.name}
+						{/* &nbsp;
+						{user.fullName.surname} */}
 					</div>
 					<div className="user__descr-status">{user.status}</div>
 				</div>
 				<div className="user__location">
-					<div className="user__location-country">{user.location.country}</div>
-					<div className="user__location-city">{user.location.city}</div>
+					<div className="user__location-country">
+						{'user.location.country'}
+					</div>
+					<div className="user__location-city">{'user.location.city'}</div>
 				</div>
 			</div>
 		</div>
