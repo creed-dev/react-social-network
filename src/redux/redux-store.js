cc9129087aee2e2b90951a55be1e4cc5374f0e93
Creed <creed-dev@internet.ru>
2021-08-13 20:37:31 +0700

    add Redux-Devtools for Chrome

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import authReducer from './auth-reducer';
 import dialogsPageReducer from './dialogsPage-reducer';
 import profilePageReducer from './profilePage-reducer';
@@ -16,8 +16,10 @@ const reducers = combineReducers({
 	app: appReducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
-
-window.store = store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+	reducers,
+	composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
 export default store;
