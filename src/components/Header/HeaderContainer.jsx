ada2b929b29cf93f27a2272161a1a819af70ac3c
Creed <creed-dev@internet.ru>
2021-08-13 17:08:22 +0700

    refactoring Header component

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -3,18 +3,11 @@ import Header from './Header';
 import { connect } from 'react-redux';
 import { isLogged, logout } from '../../redux/auth-reducer';
 
-class HeaderContainer extends React.Component {
-	render() {
-		return (
-			<Header
-				{...this.props}
-				isAuth={this.props.isAuth}
-				login={this.props.login}
-				logout={this.props.logout}
-			/>
-		);
-	}
-}
+const HeaderContainer = props => {
+	return (
+		<Header isAuth={props.isAuth} login={props.login} logout={props.logout} />
+	);
+};
 
 const mapStateToProps = state => {
 	return {
